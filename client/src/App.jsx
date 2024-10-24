8810e79f5d2763df39658699e5945bd61d7d670d
yasira2000 <yasirapunsith@gmail.com>
2024-10-24 23:39:18 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hi</h1>
+      <Button className="bg-red-500">Check</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-type", "Authorization"],
+});
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
+// database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb is connected"))
+  .catch((e) => console.log(e));
+
+
+  
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
