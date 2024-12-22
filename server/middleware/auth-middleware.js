fc001e6069020e2769ce06c57afd03c02969bdb7
yasira2000 <yasirapunsith@gmail.com>
2024-12-22 21:01:19 +0530

    auth-middleware created

---
+++ server/middleware/auth-middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (token, secretkey) => {
+    return jwt.verify(token, secretkey);
+}
+
+const authenticate = (req, res) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "User is not authenticated",
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const payload = verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
+    
+    req.user = payload;
+
+    next()
+}
+
+module.exports = authenticate;
--- server/routes/auth-routes/index.js
+++ server/routes/auth-routes/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const { registerUser } = require("../../controllers/auth-controller/index");
+const {
+  registerUser,
+  loginUser,
+} = require("../../controllers/auth-controller/index");
+const { authenticateMiddleware } = require("../../middleware/auth-middleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/check-auth", authenticateMiddleware, (req, res) => {
+    const user = req.user 
+
+    res.status(200).json({
+        success: true,
+        message: 'Authenticated user!',
+        data: {
+            user
+        }
+    })
+});
 
 module.exports = router;
