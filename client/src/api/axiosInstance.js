f49fe9eff85d4f4f1817203b74dda8bbd5476a3a
yasira2000 <yasirapunsith@gmail.com>
2024-12-23 07:22:50 +0530

    axios intercepter for request created

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,4 +2,17 @@ import axios from "axios";
 
 const axiosInstance = axios.create({ baseURL: "http://localhost:5000" });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
 export default axiosInstance;
