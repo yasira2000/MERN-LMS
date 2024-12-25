4997d502e265458f106251436275f5c8d35f7b1d
yasira2000 <yasirapunsith@gmail.com>
2024-12-25 13:43:33 +0530

    access token store in session storage done

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -7,6 +7,10 @@ export const AuthContext = createContext(null);
 export default function AuthProvider({ children }) {
   const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
   const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+  const [auth, setAuth] = useState({
+    authenticate: false,
+    user: null,
+  });
 
   async function handleRegisterUser(event) {
     event.preventDefault();
@@ -15,6 +19,22 @@ export default function AuthProvider({ children }) {
   async function handleLoginUser(event) {
     event.preventDefault();
     const data = await loginService(signInFormData);
+
+    if (data.success) {
+      sessionStorage.setItem(
+        "accessToken",
+        JSON.stringify(data.data.accessToken)
+      );
+      setAuth({
+        authenticate: true,
+        user: data.data.user,
+      });
+    } else {
+      setAuth({
+        authenticate: false,
+        user: null,
+      });
+    }
   }
 
   return (
--- server/routes/auth-routes/index.js
+++ server/routes/auth-routes/index.js
@@ -3,22 +3,22 @@ const {
   registerUser,
   loginUser,
 } = require("../../controllers/auth-controller/index");
-const { authenticateMiddleware } = require("../../middleware/auth-middleware");
+const authenticateMiddleware = require("../../middleware/auth-middleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/check-auth", authenticateMiddleware, (req, res) => {
-    const user = req.user 
+  const user = req.user;
 
-    res.status(200).json({
-        success: true,
-        message: 'Authenticated user!',
-        data: {
-            user
-        }
-    })
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    data: {
+      user,
+    },
+  });
 });
 
 module.exports = router;
