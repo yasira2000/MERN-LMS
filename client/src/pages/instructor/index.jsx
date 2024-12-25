b11254c784ebc5ee3d3a9942ab87069165edc71d
yasira2000 <yasirapunsith@gmail.com>
2024-12-26 03:12:35 +0530

    create skeleton of dashboard tabs

--- client/src/pages/instructor/index.jsx
+++ client/src/pages/instructor/index.jsx
@@ -1,10 +1,13 @@
 import InstructorCourses from "@/components/instructor-view/courses";
 import InstructorDashboard from "@/components/instructor-view/dashboard";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { BarChart, Book, LogOut } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 export default function InstructorDashboardPage() {
+  const [activeTab, setActiveTab] = useState("dashboard");
+
   const menuItems = [
     {
       icon: BarChart,
@@ -26,10 +29,12 @@ export default function InstructorDashboardPage() {
     },
   ];
 
+  function handleLogout() {}
+
   // console.log(menuItems);
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex h-full min-h-screen bg-gray-100">
       <aside className="w-64 bg-white shadow-md hidden md:block">
         <div className="p-4">
           <h2 className="text-2xl font-bold mb-4">Instructor View</h2>
@@ -38,6 +43,11 @@ export default function InstructorDashboardPage() {
               <Button
                 className="w-full justify-start mb-2"
                 key={menuItem.value}
+                onClick={
+                  menuItem.value === "logout"
+                    ? handleLogout
+                    : () => setActiveTab(menuItem.value)
+                }
               >
                 <menuItem.icon className="mr-2 h-4 w-4" />
                 {menuItem.label}
@@ -46,6 +56,18 @@ export default function InstructorDashboardPage() {
           </nav>
         </div>
       </aside>
+      <main className="flex-1 p-8 overflow-y-auto">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
+          <Tabs value={activeTab} onValueChange={setActiveTab}>
+            {menuItems.map((menuItem) => (
+              <TabsContent value={menuItem.value}>
+                {menuItem.component !== null ? menuItem.component : null}
+              </TabsContent>
+            ))}
+          </Tabs>
+        </div>
+      </main>
     </div>
   );
 }
