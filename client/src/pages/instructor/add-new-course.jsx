ec80e4980a08cf66eef5cf9d30259af91c56079b
yasira2000 <yasirapunsith@gmail.com>
2024-12-26 11:15:45 +0530

    basic structure of create new course created

--- client/src/pages/instructor/add-new-course.jsx
+++ client/src/pages/instructor/add-new-course.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React from "react";
 
 export default function AddNewCoursePage() {
   return (
-    <div>AddNewCoursePage</div>
-  )
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-extrabold mb-5">Create a new Course</h1>
+        <Button className="text-sm tracking-wider font-bold px-8">
+          Submit
+        </Button>
+      </div>
+      <Card> 
+        <CardContent>
+          <div className="container mx-auto p-4">
+            <Tabs defaultValue="curriculum" className="space-y-4">
+              <TabsList >
+                <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
+                <TabsTrigger value="course-landing-page">
+                  Course Landing Page
+                </TabsTrigger>
+                <TabsTrigger value="settings">Settings</TabsTrigger>
+              </TabsList>
+              <TabsContent value="curriculum">Curriculum</TabsContent>
+              <TabsContent value="course-landing-page">
+                Course Landing Page
+              </TabsContent>
+              <TabsContent value="settings">Settings</TabsContent>
+            </Tabs>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
