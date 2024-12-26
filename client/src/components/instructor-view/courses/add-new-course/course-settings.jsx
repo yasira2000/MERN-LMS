1285ca0834c0f00330a948c9256cb5190ee57b33
yasira2000 <yasirapunsith@gmail.com>
2024-12-27 01:07:54 +0530

    instructor course settings functionalities created

--- client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,19 +1,54 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import { InstructorContext } from "@/context/instructor-context";
+import { mediaUploadService } from "@/services";
+import React, { useContext } from "react";
 
 export default function CourseSettings() {
+  const { CourseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
+
+  async function handleImageUploadChange(event) {
+    const selectedImage = event.target.files[0];
+
+    if (selectedImage) {
+      const imageFormData = new FormData();
+      imageFormData.append("file", selectedImage);
+
+      try {
+        const response = await mediaUploadService(imageFormData);
+
+        if (response.success) {
+          setCourseLandingFormData({
+            ...CourseLandingFormData,
+            image: response.data.url,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-3">
-          <Label>Upload Course Image</Label>
-          <Input type="file" accept="image/*" />
-        </div>
+        {CourseLandingFormData?.image ? (
+          <img src={CourseLandingFormData.image} />
+        ) : (
+          <div className="flex flex-col gap-3">
+            <Label>Upload Course Image</Label>
+            <Input
+              onChange={handleImageUploadChange}
+              type="file"
+              accept="image/*"
+            />
+          </div>
+        )}
       </CardContent>
     </Card>
   );
