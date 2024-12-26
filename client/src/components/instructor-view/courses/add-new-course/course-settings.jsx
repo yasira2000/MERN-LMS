46ce7c4eeffba3d897f36b4dd5f2f1cdd45526b8
yasira2000 <yasirapunsith@gmail.com>
2024-12-26 12:20:06 +0530

    basic instructor course settings seciton created

--- client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
 
 export default function CourseSettings() {
   return (
-    <div>CourseSettings</div>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-3">
+          <Label>Upload Course Image</Label>
+          <Input type="file" accept="image/*" />
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
