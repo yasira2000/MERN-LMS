4639fa5e5e1b7bd10ccdd8be4640ee1d0ff36ab2
yasira2000 <yasirapunsith@gmail.com>
2024-12-28 00:44:29 +0530

    edit course navigation created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -46,6 +46,16 @@ function App() {
           }
         />
         <Route
+          path="/instructor/edit-course/:courseId"
+          element={
+            <RouteGuard
+              element={<AddNewCoursePage />}
+              authenticated={auth?.authenticate}
+              user={auth?.user}
+            />
+          }
+        />
+        <Route
           path="/"
           element={
             <RouteGuard
