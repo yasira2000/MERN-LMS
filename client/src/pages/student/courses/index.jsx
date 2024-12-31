85d3e77ca46266157985d3fb47e5e85c166f414f
yasira2000 <yasirapunsith@gmail.com>
2024-12-31 20:55:20 +0530

    filter options ui created

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuRadioGroup,
@@ -6,7 +7,8 @@ import {
   DropdownMenuContent,
   DropdownMenuRadioItem,
 } from "@/components/ui/dropdown-menu";
-import { sortOptions } from "@/config";
+import { Label } from "@/components/ui/label";
+import { filterOptions, sortOptions } from "@/config";
 import { ArrowUpDownIcon } from "lucide-react";
 import React, { useState } from "react";
 
@@ -18,7 +20,26 @@ export default function StudentViewCoursesPage() {
       <h1 className="text-3xl font-bold mb-4 ">All Courses</h1>
       <div className="flex flex-col md:flex-row gap-4">
         <aside className="w-full md:w-64 space-y-4">
-          <div className="p-4 space-y-4">{/* filters */}</div>
+          <div className="p-4 space-y-4">
+            {Object.keys(filterOptions).map((keyItem) => (
+              <div className="p-4 space-y-4">
+                <h3 className="font-bold mb-3">{keyItem.toUpperCase()}</h3>
+                <div className="grid gap-2 mt-2">
+                  {filterOptions[keyItem].map((option) => (
+                    <Label className="flex font-medium items-center gap-3">
+                      <Checkbox
+                        checked={false}
+                        onCheckedChange={() =>
+                          handleFilterOnChange(keyItem, option.id)
+                        }
+                      />
+                      {option.label}
+                    </Label>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </aside>
         <main className="flex-1">
           <div className="flex justify-end items-center mb-4 gap-5">
