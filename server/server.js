4d1e1ea993f44ee074f86b21b6e534303980c231
yasira2000 <yasirapunsith@gmail.com>
2024-12-31 18:13:23 +0530

    course-routes for student created

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
+const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -39,6 +40,7 @@ mongoose
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
+app.use("/student/course", studentViewCourseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
