53f4cbf6612b607dccd0c5150c730f87a3cb52ad
yasira2000 <yasirapunsith@gmail.com>
2024-12-31 21:32:50 +0530

    course display seciton ui created

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
@@ -9,24 +10,42 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
 import { filterOptions, sortOptions } from "@/config";
+import { StudentContext } from "@/context/sudent-context";
+import { fetchStudentViewCoursesListService } from "@/services";
 import { ArrowUpDownIcon } from "lucide-react";
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 export default function StudentViewCoursesPage() {
   const [sort, setSort] = useState("");
+  const { studentViewCoursesList, setStudentViewCoursesList } =
+    useContext(StudentContext);
+
+  async function fetchStudentAllViewCourses() {
+    const response = await fetchStudentViewCoursesListService();
+    if (response.success) {
+      setStudentViewCoursesList(response.data);
+    }
+  }
+
+  useEffect(() => {
+    fetchStudentAllViewCourses();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4 ">All Courses</h1>
       <div className="flex flex-col md:flex-row gap-4">
         <aside className="w-full md:w-64 space-y-4">
-          <div className="p-4 space-y-4">
+          <div className="space-y-4">
             {Object.keys(filterOptions).map((keyItem) => (
-              <div className="p-4 space-y-4">
+              <div key={keyItem} className="p-4 space-y-4">
                 <h3 className="font-bold mb-3">{keyItem.toUpperCase()}</h3>
                 <div className="grid gap-2 mt-2">
                   {filterOptions[keyItem].map((option) => (
-                    <Label className="flex font-medium items-center gap-3">
+                    <Label
+                      key={option.label}
+                      className="flex font-medium items-center gap-3"
+                    >
                       <Checkbox
                         checked={false}
                         onCheckedChange={() =>
@@ -70,6 +89,45 @@ export default function StudentViewCoursesPage() {
                 </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
+            <span className="text-sm text-black font-bold">10 results</span>
+          </div>
+          <div className="space-y-4">
+            {studentViewCoursesList && studentViewCoursesList.length > 0 ? (
+              studentViewCoursesList.map((courseItem) => (
+                <Card key={courseItem._id} className="cursor-pointer">
+                  <CardContent className="flex gap-4 p-4">
+                    <div className="w-48 h-32 flex-shrink-0">
+                      <img
+                        src={courseItem.image}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                    <div>
+                      <CardTitle className="text-xl mb-2">
+                        {courseItem.title}
+                      </CardTitle>
+                      <p className="text-[12px] text-gray-400 mb-1">
+                        Created By{" "}
+                        <span className="font-bold text-gray-600">
+                          {courseItem.instructorName}
+                        </span>
+                      </p>
+                      <p className="text-[14px] text-gray-400 mb-2">
+                        {`${courseItem.curriculum.length}
+                          ${
+                            courseItem.curriculum.length <= 1
+                              ? "Lecture"
+                              : "Lectures"
+                          } - ${courseItem.level.toUpperCase()} Level`}
+                      </p>
+                      <p className="font-bold text-lg">${courseItem.pricing}</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))
+            ) : (
+              <h1>No Courses Found</h1>
+            )}
           </div>
         </main>
       </div>
