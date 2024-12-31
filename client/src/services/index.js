fc759be6d26be2b72112041425d3a3ab97fd1de5
yasira2000 <yasirapunsith@gmail.com>
2024-12-31 14:27:37 +0530

    mediaBulkUploadService created

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -65,3 +65,16 @@ export async function updateCourseByIdService(id, formData) {
 
   return data;
 }
+
+export async function mediaBulkUploadService(formData, onProgressCallback) {
+  const { data } = await axiosInstance.post("/media/bulk-upload", formData, {
+    onUploadProgress: (progressEvent) => {
+      const percentCompleted = Math.round(
+        (progressEvent.loaded * 100) / progressEvent.total
+      );
+      onProgressCallback(percentCompleted);
+    },
+  });
+
+  return data;
+}
