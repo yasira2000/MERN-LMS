8860c742247b5b7046a8e08e58b4fa4b2b10e77d
yasira2000 <yasirapunsith@gmail.com>
2024-12-31 17:16:43 +0530

    first section of student homepage created

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,19 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { AuthContext } from "@/context/auth-context";
 import React, { useContext } from "react";
+import banner from "../../../assets/banner-img.png";
 
 export default function StudentHomePage() {
-  const { resetCredentials } = useContext(AuthContext);
-
-  function handleLogout() {
-    resetCredentials();
-    sessionStorage.clear();
-  }
-
   return (
-    <div>
-      StudentHomePage
-      <Button onClick={handleLogout}>Logout</Button>
+    <div className="min-h-screen bg-white">
+      <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+        <div className="lg:w-1/2 lg:pr-12 ">
+          <h1 className="text-4xl font-bold mb-4">Learning that gets you</h1>
+          <p className="text-xl">
+            Skills for your present and your future. Get Started with us.
+          </p>
+        </div>
+        <div className="lg:w-full mb-8 lg:mb-0">
+          <img
+            src={banner}
+            width={600}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
+      </section>
     </div>
   );
 }
