515c572bcf348fcc08a191040ea94040bbffff50
yasira2000 <yasirapunsith@gmail.com>
2025-01-01 23:42:48 +0530

    Course Title and info section created

--- client/src/pages/student/course-details/index.jsx
+++ client/src/pages/student/course-details/index.jsx
@@ -1,6 +1,7 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { StudentContext } from "@/context/sudent-context";
 import { fetchStudentViewCoursesDetailsService } from "@/services";
+import { Globe } from "lucide-react";
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -14,7 +15,7 @@ export default function StudentViewCourseDetailsPage() {
     setLoadingState,
   } = useContext(StudentContext);
 
-  const { params } = useParams();
+  const { id } = useParams();
 
   async function fetchStudentViewCoursesDetails() {
     const response = await fetchStudentViewCoursesDetailsService(
@@ -46,5 +47,28 @@ export default function StudentViewCourseDetailsPage() {
     return <Skeleton />;
   }
 
-  return <div>StudentViewCourseDetailsPage</div>;
+  return (
+    <div className="container mx-auto p-4">
+      <div className="bg-gray-800 text-white p-8 rounded-t-lg">
+        <h1 className="text-3xl font-bold mb-4">
+          {studentViewCourseDetails.title}
+        </h1>
+        <p className="text-xl mb-4">{studentViewCourseDetails.subtitle}</p>
+        <div className="flex items-center space-x-4 mt-2 text-sm">
+          <span>Created By {studentViewCourseDetails.instructorName}</span>
+          <span>Created On {studentViewCourseDetails.date.split("T")[0]}</span>
+          <span className="flex items-center">
+            <Globe className="mr-1 h-4 w-4" />
+            {studentViewCourseDetails.primaryLanguage}
+          </span>
+          <span>
+            {studentViewCourseDetails.students.length}{" "}
+            {studentViewCourseDetails.students.length > 1
+              ? "Students"
+              : "Student"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
