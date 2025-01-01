68cdb75371f128a4ee5b4737cea76795e953d7ac
yasira2000 <yasirapunsith@gmail.com>
2025-01-01 14:19:14 +0530

    getAllStudentViewCourses endpoint logic updated to perform filtering and sorting

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -2,7 +2,45 @@ const Course = require("../../models/Course");
 
 const getAllStudentViewCourses = async (req, res) => {
   try {
-    const coursesList = await Course.find({});
+    const {
+      category = [],
+      level = [],
+      primaryLanguage = [],
+      sortBy = "price-lowtohigh",
+    } = req.query;
+
+    let filters = {};
+    if (category.length) {
+      filters.category = { $in: category.split(",") };
+    }
+    if (level.length) {
+      filters.level = { $in: level.split(",") };
+    }
+    if (primaryLanguage.length) {
+      filters.primaryLanguage = { $in: primaryLanguage.split(",") };
+    }
+
+    let sortParams = {};
+
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sortParams.pricing = 1;
+        break;
+      case "price-hightolow":
+        sortParams.pricing = -1;
+        break;
+      case "title-atoz":
+        sortParams.title = 1;
+        break;
+      case "title-ztoa":
+        sortParams.title = -1;
+        break;
+      default:
+        sortParams.pricing = 1;
+        break;
+    }
+
+    const coursesList = await Course.find(filters).sortParams(sort);
 
     if (coursesList.length === 0) {
       return res.status(404).json({
@@ -38,11 +76,11 @@ const getStudentViewCourseDetails = async (req, res) => {
         data: null,
       });
     } else {
-        res.status(200).json({
-            success: true,
-            message: "Course details retrieved successfully!",
-            data: courseDetails,
-          });
+      res.status(200).json({
+        success: true,
+        message: "Course details retrieved successfully!",
+        data: courseDetails,
+      });
     }
   } catch (error) {
     console.log(error);
@@ -53,4 +91,4 @@ const getStudentViewCourseDetails = async (req, res) => {
   }
 };
 
-module.exports = {getAllStudentViewCourses, getStudentViewCourseDetails}
+module.exports = { getAllStudentViewCourses, getStudentViewCourseDetails };
