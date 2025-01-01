baa45f54232bde0133de7e113544c13e8cbd4a3d
yasira2000 <yasirapunsith@gmail.com>
2025-01-01 23:30:15 +0530

    sttudenViewCourseDetails fetching functionalitiy created

--- client/src/context/sudent-context/index.jsx
+++ client/src/context/sudent-context/index.jsx
@@ -5,6 +5,9 @@ export const StudentContext = createContext(null);
 export default function StudentProvider({ children }) {
   const [studentViewCoursesList, setStudentViewCoursesList] = useState([]);
   const [loadingState, setLoadingState] = useState(true);
+  const [studentViewCourseDetails, setStudentViewCourseDetails] =
+    useState(null);
+  const [currentCourseDetailsId, setCurrentCourseDetailsId] = useState(null);
 
   return (
     <StudentContext.Provider
@@ -13,6 +16,10 @@ export default function StudentProvider({ children }) {
         setStudentViewCoursesList,
         loadingState,
         setLoadingState,
+        studentViewCourseDetails,
+        setStudentViewCourseDetails,
+        currentCourseDetailsId,
+        setCurrentCourseDetailsId,
       }}
     >
       {children}
--- client/src/pages/student/course-details/index.jsx
+++ client/src/pages/student/course-details/index.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { StudentContext } from "@/context/sudent-context";
+import { fetchStudentViewCoursesDetailsService } from "@/services";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function StudentViewCourseDetailsPage() {
+  const {
+    studentViewCourseDetails,
+    setStudentViewCourseDetails,
+    currentCourseDetailsId,
+    setCurrentCourseDetailsId,
+    loadingState,
+    setLoadingState,
+  } = useContext(StudentContext);
+
+  const { params } = useParams();
+
+  async function fetchStudentViewCoursesDetails() {
+    const response = await fetchStudentViewCoursesDetailsService(
+      currentCourseDetailsId
+    );
+
+    if (response.success) {
+      setStudentViewCourseDetails(response.data);
+      setLoadingState(false);
+    } else {
+      setStudentViewCourseDetails(null);
+      setLoadingState(false);
+    }
+  }
+
+  useEffect(() => {
+    if (currentCourseDetailsId !== null) {
+      fetchStudentViewCoursesDetails();
+    }
+  }, [currentCourseDetailsId]);
+
+  useEffect(() => {
+    if (id) {
+      setCurrentCourseDetailsId(id);
+    }
+  }, [id]);
+
+  if (loadingState) {
+    return <Skeleton />;
+  }
+
   return <div>StudentViewCourseDetailsPage</div>;
 }
