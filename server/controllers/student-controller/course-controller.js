28f8dc8f6cd633ed404b36640b99c27436b1c51f
yasira2000 <yasirapunsith@gmail.com>
2025-01-01 14:32:11 +0530

    remove 404 status scenario from the logic

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -40,21 +40,13 @@ const getAllStudentViewCourses = async (req, res) => {
         break;
     }
 
-    const coursesList = await Course.find(filters).sortParams(sort);
+    const coursesList = await Course.find(filters).sort(sortParams);
 
-    if (coursesList.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: "No course found",
-        data: [],
-      });
-    } else {
-      res.status(200).json({
-        success: true,
-        message: "Courses retrieved successfully!",
-        data: coursesList,
-      });
-    }
+    res.status(200).json({
+      success: true,
+      message: "Courses retrieved successfully!",
+      data: coursesList,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
