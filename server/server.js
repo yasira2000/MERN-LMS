72f6982b9d0fcd644e92cac3888925dd24a5590b
yasira2000 <yasirapunsith@gmail.com>
2025-01-07 18:28:47 +0530

    Configure student courses route in server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
+const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -43,6 +44,7 @@ app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentViewCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
+app.use("/student/courses", studentCoursesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
