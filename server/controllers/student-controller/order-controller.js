f5eb114f5167001e47593369d22501e18c03205a
yasira2000 <yasirapunsith@gmail.com>
2025-01-07 13:19:51 +0530

    Create capturePayment functionality

--- server/controllers/student-controller/order-controller.js
+++ server/controllers/student-controller/order-controller.js
@@ -107,6 +107,73 @@ const createOrder = async (req, res) => {
 
 const capturePayment = async (req, res) => {
   try {
+    const { paymentId, payerId, orderId } = req.body;
+    let order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order can not be found",
+      });
+    }
+    order.paymentStatus = "paid";
+    order.orderStatus = "confirmed";
+    order.payerId = paymentId;
+    order.payerId = payerId;
+
+    await order.save();
+
+    // update student course model
+    const studentCourses = await StudentCourses.findOne({
+      userId: order.userId,
+    });
+
+    if (studentCourses) {
+      studentCourses.courses.push({
+        courseId: order.courseId,
+        title: order.courseTitle,
+        instructorId: order.instructorId,
+        instructorName: order.instructorName,
+        dateOfPurchase: order.orderDate,
+        courseImage: order.courseImage,
+      });
+
+      await studentCourses.save();
+    } else {
+      const newStudentCourses = new StudentCourses({
+        userId: order.userId,
+        courses: [
+          {
+            courseId: order.courseId,
+            title: order.courseTitle,
+            instructorId: order.instructorId,
+            instructorName: order.instructorName,
+            dateOfPurchase: order.orderDate,
+            courseImage: order.courseImage,
+          },
+        ],
+      });
+
+      newStudentCourses.save();
+    }
+
+    // update the course schema students
+    await Course.findByIdAndUpdate(order.courseId, {
+      $addToSet: {
+        students: {
+          studentId: order.userId,
+          studentName: order.userName,
+          studentEmail: order.userEmail,
+          paidAmount: order.coursePricing,
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Order Confirmed",
+      data: order,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
