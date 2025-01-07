e357ce751d00ac15544fc4a24e6df0593e7daed8
yasira2000 <yasirapunsith@gmail.com>
2025-01-08 01:53:27 +0530

    Create StudentCoursesPage

--- client/src/pages/student/student-courses/index.jsx
+++ client/src/pages/student/student-courses/index.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { AuthContext } from "@/context/auth-context";
+import { StudentContext } from "@/context/sudent-context";
+import { fetchStudentBoughtCoursesService } from "@/services";
+import { Watch } from "lucide-react";
+import React, { useContext, useEffect } from "react";
 
 export default function StudentCoursesPage() {
+  const { studentBoughtCoursesList, setStudentBoughtCoursesList } =
+    useContext(StudentContext);
+  const { auth } = useContext(AuthContext);
+
+  async function fetchStudentBoughtCourses() {
+    const response = await fetchStudentBoughtCoursesService(auth.user._id);
+
+    if (response.success) {
+      setStudentBoughtCoursesList(response.data);
+    }
+  }
+
+  useEffect(() => {
+    fetchStudentBoughtCourses();
+  }, []);
+
   return (
-    <div>StudentCoursesList</div>
-  )
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-8 ">My Courses</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {studentBoughtCoursesList && studentBoughtCoursesList.length > 0 ? (
+          studentBoughtCoursesList.map((course) => (
+            <Card key={course.id} className="flex flex-col">
+              <CardContent className="p-4 flex-grow">
+                <img
+                  src={course.courseImage}
+                  alt={course.title}
+                  className="h-52 w-full object-cover rounded-md mb-4"
+                />
+                <h3 className="font-bold mb-1">{course.title}</h3>
+                <p className="text-sm text-gray-700 mb-2">
+                  {course.instructorName}
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="flex-1">
+                  <Watch className="mr-2 h-4 w-4" />
+                  Start Watching
+                </Button>
+              </CardFooter>
+            </Card>
+          ))
+        ) : (
+          <h1 className="text-3xl font-bold">No Courses Found</h1>
+        )}
+      </div>
+    </div>
+  );
 }
