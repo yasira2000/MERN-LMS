80dcc642e9fcfd49f1175f4f7a234d45256204b2
yasira2000 <yasirapunsith@gmail.com>
2025-01-08 16:41:20 +0530

    Create basic StudentViewCourseProgressPage

---
+++ client/src/pages/student/course-progress/index.jsx
@@ -0,0 +1,98 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { AuthContext } from "@/context/auth-context";
+import { StudentContext } from "@/context/sudent-context";
+import { getCurrentCourseProgressService } from "@/services";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { ChevronLeft } from "lucide-react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function StudentViewCourseProgressPage() {
+  const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+  const { studentCurrentCourseProgress, setStudentCurrentCourseProgress } =
+    useContext(StudentContext);
+  const [lockCourse, setLockCourse] = useState(false);
+  const [curentLecture, setCurentLecture] = useState(null);
+  const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
+    useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  const { id } = useParams();
+
+  async function fetchCurrentCourseProgress() {
+    const response = await getCurrentCourseProgressService(auth.user._id, id);
+
+    if (response.success) {
+      if (!response.isPurchased) {
+        setLockCourse(true);
+      } else {
+        setStudentCurrentCourseProgress({
+          courseDetails: response.data.courseDetails,
+          progress: response.data.progress,
+        });
+        if (response.data.completed) {
+          setCurentLecture(response.data.courseDetails.curriculum[0]);
+          setShowCourseCompleteDialog(true);
+          setShowConfetti(true);
+
+          return;
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrentCourseProgress();
+  }, [id]);
+
+  return (
+    <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
+      <div className="flex items-center justify-between p-4 bg-[#1c1d1f] border-b border-gray-700">
+        <div className="flex items-center space-x-4">
+          <Button
+            onClick={() => navigate("/student-courses")}
+            className="text-black bg-white"
+            variant="ghost"
+            size="sm"
+          >
+            <ChevronLeft className="h-4 w-4 mr-2" />
+            Back to My Course Page
+          </Button>
+          <h1 className=""></h1>
+        </div>
+      </div>
+      <Dialog open={lockCourse}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>You Can't view this page</DialogTitle>
+            <DialogDescription>
+              Please purchase this course to get access
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showCourseCompleteDialog}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Congratulations!</DialogTitle>
+            <DialogDescription className="flex flex-col gap-3">
+              <Label>You have completed the course</Label>
+              <div className="flex flex-row gap-3">
+                <Button>My Course Page</Button>
+                <Button>Rewatch Course</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
