e8528726514ef9a81f2a1da4d06364bfd9a4ea8d
yasira2000 <yasirapunsith@gmail.com>
2025-01-08 14:06:08 +0530

    Configure course-progress routes in server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const instructorCourseRoutes = require("./routes/instructor-routes/course-routes
 const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -45,6 +46,7 @@ app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentViewCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
+app.use("/student/course-progress", studentCourseProgressRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
