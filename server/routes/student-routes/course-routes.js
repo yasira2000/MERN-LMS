f0f80a490f43636abfb53566a0c615418bc5d413
yasira2000 <yasirapunsith@gmail.com>
2025-01-08 13:00:20 +0530

    Add purchase-info route

--- server/routes/student-routes/course-routes.js
+++ server/routes/student-routes/course-routes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   getStudentViewCourseDetails,
   getAllStudentViewCourses,
+  checkCoursePurchasedInfo,
 } = require("../../controllers/student-controller/course-controller");
 
 const router = express.Router();
 
 router.get("/get", getAllStudentViewCourses);
-router.get("/get/details/:id/:studentId", getStudentViewCourseDetails);
+router.get("/get/details/:id", getStudentViewCourseDetails);
+router.get("/purchase-info/:id/:studentId", checkCoursePurchasedInfo);
 
 module.exports = router;
