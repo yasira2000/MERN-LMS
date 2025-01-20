c6443af0840d1a9f4811a315b7189150ae3095a0
yasira2000 <yasirapunsith@gmail.com>
2025-01-20 23:53:32 +0530

    render next course video

--- client/src/pages/student/course-progress/index.jsx
+++ client/src/pages/student/course-progress/index.jsx
@@ -6,11 +6,12 @@ import {
   DialogHeader,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
+import VideoPlayer from "@/components/video-player";
 import { AuthContext } from "@/context/auth-context";
 import { StudentContext } from "@/context/sudent-context";
 import { getCurrentCourseProgressService } from "@/services";
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import React, { useContext, useEffect, useState } from "react";
 import ReactConfetti from "react-confetti";
 import { useNavigate, useParams } from "react-router-dom";
@@ -25,11 +26,14 @@ export default function StudentViewCourseProgressPage() {
   const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
     useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
   const { id } = useParams();
 
   async function fetchCurrentCourseProgress() {
     const response = await getCurrentCourseProgressService(auth.user._id, id);
+
+    console.log(response);
 
     if (response.success) {
       if (!response.data.isPurchased) {
@@ -46,6 +50,11 @@ export default function StudentViewCourseProgressPage() {
 
           return;
         }
+        if (response.data.progress.length === 0) {
+          setCurentLecture(response.data.courseDetails.curriculum[0]);
+        } else {
+          // later
+        }
       }
     }
   }
@@ -56,9 +65,9 @@ export default function StudentViewCourseProgressPage() {
 
   useEffect(() => {
     if (showConfetti) {
-       setTimeout(()=>setShowConfetti(false), 5000)
-     }
-  }, [showConfetti])
+      setTimeout(() => setShowConfetti(false), 5000);
+    }
+  }, [showConfetti]);
 
   return (
     <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
@@ -74,7 +83,29 @@ export default function StudentViewCourseProgressPage() {
             <ChevronLeft className="h-4 w-4 mr-2" />
             Back to My Course Page
           </Button>
-          <h1 className=""></h1>
+          <h1 className="text-lg font-bold hidden md:block">
+            {studentCurrentCourseProgress.courseDetails.title}
+          </h1>
+        </div>
+        <Button>
+          {isSideBarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+      <div className="flex flex-1 overflow-hidden">
+        <div
+          className={`flex-1 ${
+            isSideBarOpen ? "mr-[400px]" : ""
+          } transition-all duration-300`}
+        >
+          <VideoPlayer
+            width="100%"
+            height="500px"
+            url={curentLecture.videoUrl}
+          />
         </div>
       </div>
       <Dialog open={lockCourse}>
