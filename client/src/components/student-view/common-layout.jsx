7d773349fc1de4791761a37f8ac55d6f3d1f00a3
yasira2000 <yasirapunsith@gmail.com>
2025-01-21 23:46:45 +0530

    Update common layout header showing logic

--- client/src/components/student-view/common-layout.jsx
+++ client/src/components/student-view/common-layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import StudentViewCommonHeader from "./header";
 
 export default function StudentViewCommonLayout() {
+  const location = useLocation();
   return (
     <div>
-      <StudentViewCommonHeader/>
+      {!location.pathname.includes("course-progress") ? (
+        <StudentViewCommonHeader />
+      ) : null}
       <Outlet />
     </div>
   );
