317ba16f72ff3b91f152a2524f857af5a13fa3a3
yasira2000 <yasirapunsith@gmail.com>
2025-01-21 23:48:35 +0530

    Add course content and Overview ui

--- client/src/pages/student/course-progress/index.jsx
+++ client/src/pages/student/course-progress/index.jsx
@@ -6,12 +6,14 @@ import {
   DialogHeader,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import VideoPlayer from "@/components/video-player";
 import { AuthContext } from "@/context/auth-context";
 import { StudentContext } from "@/context/sudent-context";
 import { getCurrentCourseProgressService } from "@/services";
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Play } from "lucide-react";
 import React, { useContext, useEffect, useState } from "react";
 import ReactConfetti from "react-confetti";
 import { useNavigate, useParams } from "react-router-dom";
@@ -22,7 +24,7 @@ export default function StudentViewCourseProgressPage() {
   const { studentCurrentCourseProgress, setStudentCurrentCourseProgress } =
     useContext(StudentContext);
   const [lockCourse, setLockCourse] = useState(false);
-  const [curentLecture, setCurentLecture] = useState(null);
+  const [currentLecture, setCurrentLecture] = useState(null);
   const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
     useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
@@ -49,7 +51,7 @@ export default function StudentViewCourseProgressPage() {
           return;
         }
         if (response.data.progress.length === 0) {
-          setCurentLecture(response.data.courseDetails.curriculum[0]);
+          setCurrentLecture(response.data.courseDetails.curriculum[0]);
         } else {
           // later
         }
@@ -87,7 +89,7 @@ export default function StudentViewCourseProgressPage() {
               : studentCurrentCourseProgress.courseDetails.title}
           </h1>
         </div>
-        <Button>
+        <Button onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
           {isSideBarOpen ? (
             <ChevronRight className="h-5 w-5" />
           ) : (
@@ -104,10 +106,65 @@ export default function StudentViewCourseProgressPage() {
           <VideoPlayer
             width="100%"
             height="500px"
-            url={curentLecture?.videoUrl}
+            url={currentLecture?.videoUrl}
+            onProgressUpdate={setCurrentLecture}
+            progressData={currentLecture}
           />
+          <div className="p-6 bg-[#1c1d1f]">
+            <h2 className="text-2xl font-bold mb-2">{currentLecture?.title}</h2>
+          </div>
+        </div>
+        <div
+          className={`fixed top-[72px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-l border-gray-700 transition-all duration-300 ${
+            isSideBarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <Tabs defaultValue="content" className="h-full flex flex-col">
+            <TabsList className="grid bg-[#1c1d1f] w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger value="content" className="rounded-none h-full">
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger value="overview" className="rounded-none h-full">
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className="h-full">
+                <div className="p-4 space-y-4">
+                  {studentCurrentCourseProgress?.courseDetails?.curriculum.map(
+                    (item) => (
+                      <div
+                        className="flex items-center space-x-2 text-sm text-white font-bold cursor-pointer"
+                        key={item._id}
+                      >
+                        {studentCurrentCourseProgress?.progress?.find(
+                          (progressItem) => progressItem.lectureId === item._id
+                        )?.viewed ? (
+                          <Check className="h-4 w-4 text-green-500" />
+                        ) : (
+                          <Play className="h-4 w-4 " />
+                        )}
+                        <span>{item?.title}</span>
+                      </div>
+                    )
+                  )}
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About this course</h2>
+                  <p className="text-gray-400">
+                    {studentCurrentCourseProgress?.courseDetails?.description}
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
+
       <Dialog open={lockCourse}>
         <DialogContent className="sm:w-[425px]">
           <DialogHeader>
