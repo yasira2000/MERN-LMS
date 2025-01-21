0e91a6fe7a5ba2b15e2f64ac8e6df137ff1dfeb8
yasira2000 <yasirapunsith@gmail.com>
2025-01-21 23:54:14 +0530

    Create mark-lecture-viewed route

--- server/routes/student-routes/course-progress-routes.js
+++ server/routes/student-routes/course-progress-routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const {
   getCurrentCourseProgress,
+  markCurrentLectureAsViewed,
+  resetCurrentCourseProgress,
 } = require("../../controllers/student-controller/course-progress-controller");
 
 const router = express.Router();
 
 router.get("/get/:userId/:courseId", getCurrentCourseProgress);
+router.post("/mark-lecture-viewed", markCurrentLectureAsViewed)
 
 module.exports = router;
